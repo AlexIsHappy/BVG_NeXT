68da83d86eb5d0accfe03e7381484f575b0794f3
Aleksandr Shirokov <alexishappy@yandex.ru>
2016-12-04 11:55:42 +0100

    Add files via upload

---
+++ Scripts/NumPeopleText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NumPeopleText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var instruction = GetComponent<Text>();
+        instruction.text = "24";
+    }
+}
---
+++ Scripts/TestText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TestText : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var instruction = GetComponent<Text>();
+        instruction.text = "12 %";
+    }
+
+    int cntdwn1 = 3500;
+    int cntdwn2 = 4000;
+}
